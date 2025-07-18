fc23630411e0e70f78b25b0eb1fb4c7c650deafb
mintesnot <mentesnotaa@gmail.com>
2025-07-18 16:54:18 +0300

    Refactor alarm function to improve console output clarity

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,17 +1,18 @@
 from playsound import playsound
 import time
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
 
 def alarm(seconds):
     time_elapsed = 0
+    print(CLEAR + CLEAR_AND_RETURN, end='')
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed += 1
-
         time_left = seconds - time_elapsed
         minutes_left = time_left // 60
         seconds_left = time_left % 60
         print(f"Time left: {minutes_left} minutes and {seconds_left} seconds", end='\r')
-
 
 alarm(10)   
      
